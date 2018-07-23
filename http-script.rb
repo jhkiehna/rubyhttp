17f603da672b3fd4aba37f2ea4c362c6bd8843ef
jhkiehna <33327590+jhkiehna@users.noreply.github.com>
2018-07-23 15:49:34 -0400

    Update http-script.rb

--- http-script.rb
+++ http-script.rb
@@ -1,5 +1,8 @@
 require "http"
 
+#this will be changed to a post request to the correct endpoint
+#to send a signal to API to let it know when the device has been
+#restarted
 $response = HTTP.get("https://www.google.com").to_s
 
 # Write to a file for testing
