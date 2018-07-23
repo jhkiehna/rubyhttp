98cb6e2bbcd3a98aaf5baefd414f2cced96c7257
jhkiehna <33327590+jhkiehna@users.noreply.github.com>
2018-07-23 15:48:34 -0400

    Delete run-test.sh

--- run-test.sh
+++
@@ -1,5 +0,0 @@
-echo "---------------------------------------------------------" >> ./scriptlog.txt
-echo "TEST OUTPUT -- SCRIPT RAN -- SUCCESSSSSSSS!!!!!!!!!!!!!!!" >> ./scriptlog.txt
-echo date +"	DATE:	%Y-%m-%d" >> ./scriptlog.txt
-echo "---------------------------------------------------------" >> ./scriptlog.txt
-
